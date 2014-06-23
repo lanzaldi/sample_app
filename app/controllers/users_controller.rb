07e7726a14de3a16381a03a7e5490e975d9d4d7f
Laura Anzaldi <ljanzaldi@gmail.com>
2014-06-23 11:13:53 -0400

    finish user edit, update, index and destroy actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,45 @@
 class UsersController < ApplicationController
+  before_action :signed_in_user, only: [:index, :edit, :update]
+  before_action :correct_user, only: [:edit, :update]
+  before_action :admin_user, only: :destroy
+  
   def show
 	@user = User.find(params[:id])
+  end
+  
+  def destroy
+	let(:doomed_user) User.find(params[:id])
+	if current_user?(doomed_user)
+		flash[:error] = "You cannot delete yourself!"
+		redirect_to users_url		
+	else
+		doomed_user.destroy
+		flash[:success] = "User deleted"
+		redirect_to users_url
+	end
+  end
+	
+	
+  def index
+	@users = User.paginate(page: params[:page])
   end
   
   def new
 	@user = User.new
   end
+  
+  def edit
+  end
+  
+  def update
+	if @user.update_attributes(user_params)
+		flash[:success] = "Your changes were successfully saved."
+		redirect_to @user
+	else
+		render 'edit'
+	end
+  end
+	
   
   def create
 	@user = User.new(user_params)
@@ -21,5 +55,21 @@ class UsersController < ApplicationController
   private
 	def user_params
 		params.require(:user).permit(:name, :email, :password, :password_confirmation)
+	end
+	
+	def signed_in_user
+		unless signed_in?
+			store_location
+			redirect_to signin_url, notice: "Please sign in."
+		end
+	end
+	
+	def correct_user
+		@user = User.find(params[:id])
+		redirect_to root_url, notice: "You are not authorized to access that page." unless current_user?(@user)
+	end
+	
+	def admin_user
+		redirect_to(root_url) unless current_user.admin?
 	end
 end
